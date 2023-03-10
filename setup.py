7cee16402d30c9dd9fa0de498911cc55c93f96d0
Paola Diaz <paola.dgt@gmail.com>
2023-03-10 09:09:20 +0100

    setup_app

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="risk_assesment_app",
     version="0.0.1",
-    python_requires=">=3.9",
+    python_requires=">=3.8",
     packages=find_packages(),
     author="Paola DGT",
     author_email="paola.dgt@gmail.com",
