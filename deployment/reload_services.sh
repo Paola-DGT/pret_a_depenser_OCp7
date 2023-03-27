e644472a1d9559204a5cf612129487a39ce41170
Paola Diazgomez <paola.dgt@gmail.com>
2023-03-27 23:07:28 +0200

    update and fix deployment

--- deployment/reload_services.sh
+++ deployment/reload_services.sh
@@ -14,22 +14,33 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap check_code_on_exit EXIT
 
 # set server specific variables -all paths absolute-
-apps=(mlflow-server backend dashboard)    # my app components service names
-app_root_path="/volume/p7svr/deployment"  # app path on server
-systemd_path="/etc/systemd/system"        # systemd path on server
-nginx_conf_path="/etc/nginx"              # nginx conf path on server
+venvs=(gunicorn-venv streamlit-venv)          # venvs names
+apps=(backend dashboard)                      # my app service names
+app_root_path="/volume/p7svr"                 # app path on server
+deploy_root_path="/volume/p7svr/deployment"   # deploy path on server
+systemd_path="/etc/systemd/system"            # systemd path on server
+nginx_conf_path="/etc/nginx"                  # nginx conf path on server
 
 # copy nginx config
-sudo cp -f $app_root_path/nginx.conf $nginx_conf_path/nginx.conf
+sudo cp -f $deploy_root_path/nginx.conf $nginx_conf_path/nginx.conf
 
 echo "Nginx conf file copied"
 
 # copy all unit files to systemd directory
 for app in "${apps[@]}"; do
-    sudo cp -f $app_root_path/$app.service $systemd_path/$app.service || exit 1
+    sudo cp -f $deploy_root_path/$app.service $systemd_path/$app.service || exit 1
 done
 
-echo "App Unit files from $app_root_path/ were copied to $systemd_path/"
+echo "App Unit files from $deploy_root_path/ were copied to $systemd_path/"
+
+# reinstall app in venvs
+echo "Re-installing virtual environements"
+
+for venv in "${venvs[@]}"; do
+    /volume/$venv/bin/python -m pip install --force-reinstall -e $app_root_path/. || exit 1
+done
+
+echo "Virtual environements succesfully installed !"
 
 # reload daemon after file changes
 sudo systemctl daemon-reload
